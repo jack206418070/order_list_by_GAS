a96e7b1fbe0b8946f90c4fc7b7a1cd0ebc9ff1e5
jack206418070 <44224865+jack206418070@users.noreply.github.com>
2021-07-03 06:37:58 +0800

    update table

--- js/table.js
+++ js/table.js
@@ -19,7 +19,7 @@ function getListInfo() {
             // let beef = [];
             let date = new Date();
             res.forEach((item, index) => {
-                if (item.data[0] == `${date.getMonth() + 1}/${date.getDate()+1}`) {
+                if (item.data[0] == `${date.getMonth() + 1}/${date.getDate()}`) {
                     let productList = item.data[8];
                     let productListTwo = [];
                     productList = productList.split(/[\n]/);
